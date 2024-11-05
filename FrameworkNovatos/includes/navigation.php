f8ed60dbeeb250d319551aa627b44f986d1ed93b
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2024-11-05 12:55:56 +0100

    changes navigation

--- FrameworkNovatos/includes/navigation.php
+++ FrameworkNovatos/includes/navigation.php
@@ -3,11 +3,11 @@
     <a class="navbar-brand me-auto" href="Index.php">
       <img src="assets/img/LogoSinFondo.png" alt="Logo" id="logo">
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     
-    <div class="collapse navbar-collapse" id="mynavbar">
+    <div class="collapse navbar-collapse" id="navbar">
       <ul class="navbar-nav ms-auto me-2">
         <li class="nav-item">
           <a class="nav-link" href="carta.php">Carta</a>
@@ -17,6 +17,9 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="delivery.php">Delivery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="delivery.php"><i class="fa-solid fa-earth-americas"></i></a>
         </li>
       </ul>
     </div>
