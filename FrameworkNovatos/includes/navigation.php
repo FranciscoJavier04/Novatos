62d1c987e54486590e0489a5e4b25a6496ef99bc
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2024-11-05 12:42:02 +0100

    cambios navbar

--- FrameworkNovatos/includes/navigation.php
+++ FrameworkNovatos/includes/navigation.php
@@ -1,11 +1,14 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-<div class="container-fluid">
-    <a class="navbar-brand" href="Index.php">Logo</a>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand me-auto" href="Index.php">
+      <img src="assets/img/LogoSinFondo.png" alt="Logo" id="logo">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="mynavbar">
-      <ul class="navbar-nav me-auto">
+      <ul class="navbar-nav ms-auto me-2">
         <li class="nav-item">
           <a class="nav-link" href="carta.php">Carta</a>
         </li>
@@ -16,10 +19,6 @@
           <a class="nav-link" href="delivery.php">Delivery</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="text" placeholder="Search">
-        <button class="btn btn-primary" type="button">Search</button>
-      </form>
     </div>
   </div>
 </nav>
