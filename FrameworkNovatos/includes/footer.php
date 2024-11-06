d19bbf6c5a7fabb00caa53110e5fb331163890b0
Alexro-03 <arodram24@iesmarquesdecomares.org>
2024-11-06 09:31:06 +0100

    cambios footer

--- FrameworkNovatos/includes/footer.php
+++ FrameworkNovatos/includes/footer.php
@@ -1,63 +1,55 @@
 <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <!-- ÓNEO Y DIRECCION-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <a><h4 class="text-uppercase mb-4">ÓNEO</h4></a>
-                    
-                    <p class="descFooter">
+    <div class="container">
+        <div class="row">
+            <!-- ÓNEO y Dirección -->
+            <div class="col-lg-4 mb-4">
+                <div class="d-flex align-items-center justify-content-center mb-3">
+                    <img src="assets/img/LogoSinFondo.png" class="logo me-2">
+                    <p class="oneo mb-0">ÓNEO</p>
+                </div>
+                <p class="descFooter">
                     Óneo es un restaurante moderno y lujoso que ofrece una experiencia gastronómica exclusiva, 
-                    fusionando alta cocina con un diseño elegante y contemporáneo para crear un ambiente sofisticado.</p>
-                    
-                    <div class="d-flex align-items-center justify-content-justify ">
-                        <i class="fas fa-map-marker-alt"></i>
-                        <p class="mb-0">&nbsp Dirección: Calle Francisco Villalón nº9, Marbella/España</p>
-                    </div>
-
-                </div>
-                
-                <!-- REDES SOCIALES E INFORMACON-->
-                <div class="col-lg-4 mb-5 mb-lg-0 text-center">
-                    <h4 class="text-uppercase mb-4">Redes Sociales</h4>
-                    
-                    <div class="d-flex align-items-center justify-content-center mb-2">
-                        <a ><i class="fab fa-instagram"></i></a>
-                        <p class="mb-0">&nbsp Óneo_Official</p>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-center mb-2">
-                        <a ><i class="fab fa-facebook"></i></a>
-                        <p class="mb-0">&nbsp Óneo_Official</p>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-center mb-2">
-                        <a><i class="fab fa-telegram"></i></a>
-                        <p class="mb-0">&nbsp Óneo_Official</p>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-center mb-2">
-                        <a><i class="fab fa-tripadvisor"></i></a>
-                        <p class="mb-0">&nbsp Óneo_Official</p>
-                    </div>
-
-                    <h4 class="text-uppercase mb-4">Información</h4>
-                    
-                    <div class="d-flex align-items-center justify-content-center mb-2">
-                        <a><i class="fas fa-phone-alt"></i></a>
-                        <p class="mb-0">&nbsp 635582372</p>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-center">
-                        <a><i class="fas fa-envelope"></i></a>
-                        <p class="mb-0">&nbsp informacion.óneo@gmail.com</p>
-                    </div>
-                </div>
-
-
-                <!-- AVISOS Y POLITICAS DE PRIVACIDAD-->
-                <div class="col-lg-4 text-justify">
-                    <h4 class="text-uppercase mb-4">Aviso Legal</h4>
-                    <h4 class="text-uppercase mb-4">Politica de Privacidad</h4>
-                    <h4 class="text-uppercase mb-4">Politica de Cookies</h4>
-                    
+                    fusionando alta cocina con un diseño elegante y contemporáneo para crear un ambiente sofisticado.
+                </p>
+                <div class="d-flex">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p class="mb-0 ms-2">Dirección: Calle Francisco Villalón nº9, Marbella/España</p>
                 </div>
             </div>
-        </div>
-    </footer>
 
+            <!-- Redes Sociales e Información -->
+            <div class="col-lg-4 mb-4 text-center">
+                <p class="texto24 text-uppercase mb-4">Redes Sociales</p>
+                
+                <div class="d-flex align-items-center justify-content-center mb-2">
+                    <span class="social-icon me-3"><i class="fab fa-instagram"></i> Óneo_Official</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-2">
+                    <span class="social-icon"><i class="fab fa-facebook"></i> Óneo_Official</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-2">
+                    <span class="social-icon me-3"><i class="fab fa-telegram"></i> Óneo_Official</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4">
+                    <span class="social-icon"><i class="fab fa-tripadvisor"></i> Óneo_Official</span>
+                </div>
+
+                <p class="texto24 text-uppercase mb-4">Información</p>
+                
+                <div class="d-flex align-items-center justify-content-center mb-2">
+                    <span><i class="fas fa-phone-alt"></i> 635582372</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-center">
+                    <span><i class="fas fa-envelope"></i> informacion.óneo@gmail.com</span>
+                </div>
+            </div>
+
+            <!-- Aviso Legal y Políticas -->
+            <div class="col-lg-4 text-center">
+                <p class="texto24 text-uppercase mb-4">Aviso Legal</p>
+                <p class="texto24 text-uppercase mb-4">Política de Privacidad</p>
+                <p class="texto24 text-uppercase mb-4">Política de Cookies</p>
+            </div>
+        </div>
+    </div>
+</footer>
