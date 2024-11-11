6bcceb4b539b5115a67d5f9fbd8063cc6f5c2dcc
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2024-11-11 09:02:25 +0100

    bootstrap del index

--- FrameworkNovatos/index.php
+++ FrameworkNovatos/index.php
@@ -1,58 +1,65 @@
-<?php include("includes/a_config.php");?>
+<?php include("includes/a_config.php"); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<?php include("includes/head-tag-contents.php");?>
+    <?php include("includes/head-tag-contents.php"); ?>
 </head>
+
 <body>
-<?php include("includes/navigation.php");?>
-<main>
-    <div class="portfolio-main">
-        <button class="btn btn-lg btn-light">DESCÚBRENOS</button>
-    </div>
-
-    <div class="container py-5">
-        <!-- Primera tarjeta -->
-        <div class="row align-items-center mb-3">
-            <div class="col-md-4">
-                <img src="assets/img/Logo.png" alt="Imagen 1" class="img-fluid video">
-            </div>
-            <div class="col-md-8">
-                <p>
-                    Estoy seguro de que has visto el Padrino, uno de los nuestros o cualquier película de Mafiosos. Pues hoy os traigo Comiendo en el RESTAURANTE DE LA MAFIA en Benidorm.
-                </p>
-            </div>
+    <?php include("includes/navigation.php"); ?>
+    <main class="text-center">
+        <div class="portfolio-main p-5">
+            <button class="btn btn-lg btn-light">DESCÚBRENOS</button>
         </div>
-        <div class="separator"></div>
 
-        <!-- Segunda tarjeta (invertida) -->
-        <div class="row align-items-center mb-3">
-            <div class="col-md-8 order-md-1">
-                <p>
-                    Así es comer en uno de los restaurantes más lujosos de España, probamos todos los platos para dar nuestra opinión más sincera.
-                </p>
+        <div class="container py-5">
+            <!-- Primera tarjeta -->
+            <div class="row align-items-center mb-3">
+                <div class="col-md-4">
+                    <img src="assets/img/Logo.png" alt="Imagen 1" class="img-fluid video">
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        Estoy seguro de que has visto el Padrino, uno de los nuestros o cualquier película de Mafiosos.
+                        Pues hoy os traigo Comiendo en el RESTAURANTE DE LA MAFIA en Benidorm.
+                    </p>
+                </div>
             </div>
-            <div class="col-md-4 order-md-0">
-                <img src="assets/img/Logo.png" alt="Imagen 2" class="img-fluid video">
+            <div class="separator"></div>
+
+            <!-- Segunda tarjeta (invertida) -->
+            <div class="row align-items-center my-3">
+                <div class="col-md-4 order-md-1">
+                    <img src="assets/img/Logo.png" alt="Imagen 2" class="img-fluid video">
+                </div>
+                <div class="col-md-8 order-md-0">
+                    <p>
+                        Así es comer en uno de los restaurantes más lujosos de España, probamos todos los platos para
+                        dar nuestra opinión más sincera.
+                    </p>
+                </div>
+
             </div>
+            <div class="separator"></div>
+
+            <!-- Tercera tarjeta -->
+            <div class="row align-items-center my-3">
+                <div class="col-md-4">
+                    <img src="assets/img/Logo.png" alt="Imagen 3" class="img-fluid video">
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        La calidad y el buen servicio son nuestra razón de ser para que un cliente nos elija como su
+                        restaurante de referencia. Una filosofía llevada a nuestro día a día.
+                    </p>
+                </div>
+            </div>
+            <div class="separator"></div>
         </div>
-        <div class="separator"></div>
+    </main>
 
-        <!-- Tercera tarjeta -->
-        <div class="row align-items-center mb-3">
-            <div class="col-md-4">
-                <img src="assets/img/Logo.png" alt="Imagen 3" class="img-fluid video">
-            </div>
-            <div class="col-md-8">
-                <p>
-                    La calidad y el buen servicio son nuestra razón de ser para que un cliente nos elija como su restaurante de referencia. Una filosofía llevada a nuestro día a día.
-                </p>
-            </div>
-        </div>
-        <div class="separator"></div>
-    </div>
-</main>
-
-<?php include("includes/footer.php");?>
+    <?php include("includes/footer.php"); ?>
 </body>
+
 </html>
