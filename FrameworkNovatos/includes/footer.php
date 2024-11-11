06e87a868754ce18c5e49452d4861d55a380ef5b
Alexro-03 <arodram24@iesmarquesdecomares.org>
2024-11-11 09:54:11 +0100

    cambios footer

--- FrameworkNovatos/includes/footer.php
+++ FrameworkNovatos/includes/footer.php
@@ -1,8 +1,8 @@
 <footer class="footer">
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-between">
             <!-- ÓNEO y Dirección -->
-            <div class="col-lg-4 mb-4">
+            <div class="col-lg-5">
                 <div class="d-flex justify-content-center mb-3">
                     <img src="assets/img/LogoSinFondo.png" class="logo me-2">
                     <p class="oneo mb-0">ÓNEO</p>
@@ -16,39 +16,44 @@
                     <p class="mb-0 ms-2">Dirección: Calle Francisco Villalón nº9, Marbella/España</p>
                 </div>
             </div>
+
+            <div class="linea col-lg-3 mx-4">
+            </div>
+
             <!-- Redes Sociales e Información -->
-            <div class="col-lg-4 mb-4">
-                <p class="texto24 text-uppercase mb-4">Redes Sociales</p>
+            <div class="col-lg-3">
+                <p class="texto24">Redes Sociales</p>
                 
-                <div class="d-flex justify-content-center mb-2">
+                <div class="d-flex mb-2">
                     <span class="social-icon me-3"><i class="fab fa-instagram"></i> Óneo_Official</span>
                 </div>
-                <div class="d-flex justify-content-center mb-2">
+                <div class="d-flex mb-2">
                     <span class="social-icon"><i class="fab fa-facebook"></i> Óneo_Official</span>
                 </div>
-                <div class="d-flex justify-content-center mb-2">
+                <div class="d-flex mb-2">
                     <span class="social-icon me-3"><i class="fab fa-telegram"></i> Óneo_Official</span>
                 </div>
-                <div class="d-flex justify-content-center mb-4">
-                    <span class="social-icon"><i class="fab fa-tripadvisor"></i> Óneo_Official</span>
+                <div class="d-flex mb-4">
+                    <span class="social-icon"><i class="fab fa-twitter"></i> Óneo_Official</span>
                 </div>
 
-                <p class="texto24 text-uppercase mb-4">Información</p>
+                <p class="texto24">Información</p>
                 
-                <div class="d-flex justify-content-center mb-2">
+                <div class="d-flex mb-2">
                     <span><i class="fas fa-phone-alt"></i> 635582372</span>
                 </div>
-                <div class="d-flex justify-content-center">
+                <div class="d-flex">
                     <span><i class="fas fa-envelope"></i> informacion.óneo@gmail.com</span>
                 </div>
             </div>
 
             <!-- Aviso Legal y Políticas -->
-            <div class="col-lg-4 text-center">
-                <p class="texto24 text-uppercase mb-4">Aviso Legal</p>
-                <p class="texto24 text-uppercase mb-4">Política de Privacidad</p>
-                <p class="texto24 text-uppercase mb-4">Política de Cookies</p>
+            <div class="col-lg-3">
+                <a class="texto24 nav-link" id="avisoLegal" href="avisoL.php">Aviso Legal</a>
+                <a class="texto24 nav-link" id="polPri" href="politicaP.php">Política de Privacidad</a>
+                <a class="texto24 nav-link" id="polCo" href="politicaC.php">Política de Cookies</a>
             </div>
+
         </div>
     </div>
 </footer>
