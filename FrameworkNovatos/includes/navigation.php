f3bfa395cb681fe52f4ce3ced4618155064fa7b5
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2024-11-11 09:12:51 +0100

    add

--- FrameworkNovatos/includes/navigation.php
+++ FrameworkNovatos/includes/navigation.php
@@ -2,30 +2,32 @@
   <div class="container-fluid">
     <a class="navbar-brand me-auto" href="Index.php">
       <img src="assets/img/LogoSinFondo.png" alt="Logo" id="logo">
+      <span id="titulo">ÓNEO</span>
     </a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
       <i class="fas fa-bars"></i>
     </button>
-    
-    <div class="collapse navbar-collapse" id="navbar">
+
+    <div class="collapse navbar-collapse " id="navbar">
       <ul class="navbar-nav ms-auto me-2">
         <li class="nav-item">
           <a class="nav-link" href="carta.php">Carta</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="reservas.php">Reserva</a>
+          <a class="nav-link" href="reservas.php">Reservas</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="delivery.php">Delivery</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="delivery.php"><i class="fas fa-globe-europe"></i>/ES</a>
+          <a class="nav-link" href="idioma.php"><i class="fas fa-globe-europe"></i>/ES</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="delivery.php"><i class="fas fa-user"></i></a>
+          <a class="nav-link" href="usuario.php"><i class="fas fa-user"></i></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="delivery.php"><i class="fas fa-cart-plus"></i></a>
+          <a class="nav-link" href="carrito.php"><i class="fas fa-cart-plus"></i></a>
         </li>
       </ul>
     </div>
