c73426b829aed6afbbc70a8aed4c3f60b96daa51
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2024-11-13 08:52:14 +0100

    cambios

--- FrameworkNovatos/reservas.php
+++ FrameworkNovatos/reservas.php
@@ -8,10 +8,18 @@
 
 <body>
     <?php include("includes/navigation.php"); ?>
-    <main >
-       
+    <main class="align-items-center">
+        <div class="container mt-3">
+            <div class="card" style="width:400px">
+                <img class="card-img-top" src="../bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+                <div class="card-body">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    <a href="#" class="btn btn-primary">See Profile</a>
+                </div>
+            </div>
+        </div>
     </main>
-
     <?php include("includes/footer.php"); ?>
 </body>
 
