d05b721c9368fa0e13f095337bcd9c1cf7465264
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2024-11-25 10:13:02 +0100

    finalizado datos reserva(revisar scss) y comienzo confReserva

---
+++ FrameworkNovatos/confReserva.php
@@ -0,0 +1,17 @@
+<?php include("includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head-tag-contents.php"); ?>
+</head>
+
+<body id="bodyReserva">
+    <?php include("includes/navigation.php"); ?>
+    <main class="">
+        
+    </main>
+    <?php include("includes/footer.php"); ?>
+</body>
+
+</html>
