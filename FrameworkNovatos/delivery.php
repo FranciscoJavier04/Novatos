d3112f7086c0b43a83e74bbc8291080ae032f8ca
Alexro-03 <arodram24@iesmarquesdecomares.org>
2024-11-28 14:56:59 +0100

    cambios de muchas cosas

--- FrameworkNovatos/delivery.php
+++ FrameworkNovatos/delivery.php
@@ -6,14 +6,14 @@
     <?php include("includes/head-tag-contents.php"); ?>
 </head>
 
-<body class="body-del align-items-center">
+<body class="body-del align-items-center text-warning ">
 
     <?php include("includes/navigation.php"); ?>
 
-    <div class="container-del">
+    <div class="container-del p-4 d-flex">
         <div class="row">
             <!-- Sidebar de Categorías -->
-            <div class="sidebar-del col-md-3">
+            <div class="sidebar-del col-md-3 bg-warning p-3 rounded-3 text-dark">
                 <h1 class="titulo-sidebar text-center">Categorías</h1>
                 <nav class="nav flex-column align-items-center">
                     <a class="menu-item-del" href="#entrantes">Entrantes</a>
@@ -24,99 +24,99 @@
             </div>
 
             <!-- Contenido Principal -->
-            <div class="carta-del col-md-9 text-justify bg-info w-5 mx-auto rounded-3 text-paragraph">
+            <div class="carta-del col-md-9 text-justify bg-info w-5 mx-auto rounded-3 text-paragraph p-3">
                 <!-- ENTRANTES -->
                 <h1 class="subtitulo-carta text-center mb-5" id="entrantes" >Entrantes</h1>
                 <div class="row">
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
-                            <img src="assets/img/carta/Principal.webp">
+                        <div class="col-md-3">
+                            <img src="assets/img/carta/ENTRANTES.webp">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Jamón Ibérico de Bellota. 35€</span>
                             <p>Cortado a mano, acompañado de pan con tomate.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
 
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
-                            <img src="assets/img/carta/Principal.webp">
+                        <div class="col-md-3">
+                            <img src="assets/img/carta/Tartar.png">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Tartar de Atún Rojo del Mediterráneo. 28€</span>
                             <p>Con aguacate, sésamo y emulsión de wasabi.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
-                            <img src="assets/img/carta/Principal.webp" alt="Carpaccio de Wagyu">
+                        <div class="col-md-3">
+                            <img src="assets/img/carta/Carpaccio.png" alt="Carpaccio de Wagyu">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Carpaccio de Wagyu con Trufa Negra. 32€</span>
                             <p>Aliñado con aceite de oliva virgen extra y parmesano.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
-                            <img src="assets/img/carta/Principal.webp" alt="Vieiras a la Plancha">
+                        <div class="col-md-3">
+                            <img src="assets/img/carta/Vieiras.png" alt="Vieiras a la Plancha">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Vieiras a la Plancha. 29€</span>
                             <p>Con puré de coliflor, mantequilla noisette y caviar.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
-                            <img src="assets/img/carta/Principal.webp" alt="Foie Gras Mi-cuit">
+                        <div class="col-md-3">
+                            <img src="assets/img/carta/Foie.png" alt="Foie Gras Mi-cuit">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Foie Gras Mi-cuit. 29€</span>
                             <p>Servido con chutney de higos y brioche tostado.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
-                            <img src="assets/img/carta/Principal.webp" alt="Pulpo a la Brasa">
+                        <div class="col-md-3">
+                            <img src="assets/img/carta/Pulpo.png" alt="Pulpo a la Brasa">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Pulpo a la Brasa. 27€</span>
                             <p>Con puré de patatas ahumadas y pimentón de la Vera.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
-                            <img src="assets/img/carta/Principal.webp" alt="Ceviche de Lubina y Maracuyá">
+                        <div class="col-md-3">
+                            <img src="assets/img/carta/Ceviche.webp" alt="Ceviche de Lubina y Maracuyá">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Ceviche de Lubina y Maracuyá. 26€</span>
                             <p>Con leche de tigre y maíz crujiente.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
                 </div>
@@ -126,80 +126,80 @@
                 <div class="row">
 
                 <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Cochinillo Asado">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Cochinillo Asado a Baja Temperatura. 45€</span>
                             <p>Con puré de boniato y jugo de su asado.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Solomillo de Buey">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Solomillo de Buey con Salsa de Oporto. 48€</span>
                             <p>Acompañado de papas gratinadas y espárragos verdes.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Merluza a la Koskera">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Merluza a la Koskera. 42€</span>
                             <p>Con almejas, espárragos y guisantes frescos.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Arroz Negro">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Arroz Negro con Calamar y Alioli de Ajo Negro. 38€</span>
                             <p>Cremoso y con sabores profundos de tinta de calamar.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Carrilleras de Ternera">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Carrilleras de Ternera al Vino Tinto. 44€</span>
                             <p>Con parmentier de patata trufada.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Ravioli de Bogavante">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Ravioli de Bogavante. 40€</span>
                             <p>Con bisque de marisco y chips de zanahoria.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
                 </div>
@@ -208,67 +208,67 @@
                 <h1 class="subtitulo-carta mt-5 text-center mb-5" id="especialidades">Especialidades</h1>
                 <div class="row">
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Paella de Mariscos Tradicional">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Paella de Mariscos Tradicional. 50€</span>
                             <p>Con gambas, mejillones, cigalas y azafrán.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Lubina Salvaje al Horno">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Lubina Salvaje al Horno. 55€</span>
                             <p>Servida con hinojo asado y salsa de limón.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Presa Ibérica con Glaseado de Miel y Mostaza">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Presa Ibérica con Glaseado de Miel y Mostaza. 46€</span>
                             <p>Acompañada de puré de batata y espárragos.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Cordero Lechal Confitado">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Cordero Lechal Confitado. 52€</span>
                             <p>Con reducción de vino de Rioja y espinacas salteadas.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
 
                     <div class="row align-items-start mb-4">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <img src="assets/img/carta/Principal.webp" alt="Bacalao a la Brasa con Crema de Guisantes">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-6 fs-5">
                             <span class="fw-bold">Bacalao a la Brasa con Crema de Guisantes. 47€</span>
                             <p>Con cebollas caramelizadas y un toque de jamón ibérico.</p>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn-xl">AÑADIR</button>
+                            <a href="carrito.php" class="btn btn-del">AÑADIR</a>
                         </div>
                     </div>
                 </div>
