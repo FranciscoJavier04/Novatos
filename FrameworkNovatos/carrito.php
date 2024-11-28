87842025dfc716de7caf8a687c2c077a3944ede6
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2024-11-28 19:46:06 +0100

    adaptacion a movil

--- FrameworkNovatos/carrito.php
+++ FrameworkNovatos/carrito.php
@@ -11,9 +11,9 @@
     <div class="container py-5">
         <h1 class="text-center fst-italic fw-bold mb-4">CARRITO</h1>
         <div class="row text-primary mb-3">
-            <div class="col-12">
-                <div class="bg-secondary fs-5 rounded-3 p-3">
-                    <div class="row align-items-center text-center text-md-start">
+            <div class="col-12 p-0">
+                <div class="bg-secondary fs-5 rounded-3 quitar-rounded p-3">
+                    <div class="row align-items-center justify-content-center text-center text-md-start">
                         <div class="col-6 col-md-2 ">
                             <img src="/assets/img/carta/Principal.webp" class="img-fluid rounded-circle"
                                 alt="Jamón Ibérico">
@@ -38,8 +38,8 @@
         <div class="row text-primary mb-3">
             <div class="col-12">
                 <div class="bg-secondary fs-5 rounded-3 p-3">
-                    <div class="row align-items-center">
-                        <div class="col-md-2 ">
+                    <div class="row align-items-center justify-content-center text-center text-md-start">
+                        <div class="col-6 col-md-2 ">
                             <img src="/assets/img/carta/Principal.webp" class="img-fluid rounded-circle"
                                 alt="Jamón Ibérico">
                         </div>
@@ -52,7 +52,7 @@
                             <button type="submit" class="btn btn-comun">1</button>
                             <button type="submit" class="btn btn-comun">+</button>
                         </div>
-                        <div class="col-md-2 text-end">
+                        <div class="col-md-2 text-center text-md-end">
                             <p class="subtotal">SubTotal: 45€</p>
                         </div>
                     </div>
@@ -63,8 +63,8 @@
         <div class="row text-primary mb-3">
             <div class="col-12">
                 <div class="bg-secondary fs-5 rounded-3 p-3">
-                    <div class="row align-items-center">
-                        <div class="col-md-2 ">
+                    <div class="row align-items-center justify-content-center text-center text-md-start">
+                        <div class="col-6 col-md-2 ">
                             <img src="/assets/img/carta/Principal.webp" class="img-fluid rounded-circle"
                                 alt="Jamón Ibérico">
                         </div>
@@ -77,7 +77,7 @@
                             <button type="submit" class="btn btn-comun">1</button>
                             <button type="submit" class="btn btn-comun">+</button>
                         </div>
-                        <div class="col-md-2 text-end">
+                        <div class="col-md-2 text-center text-md-end">
                             <p class="subtotal">SubTotal: 45€</p>
                         </div>
                     </div>
@@ -87,7 +87,7 @@
         <div class="d-flex justify-content-center fst-italic align-items-center ">
             <div class="text-primary rounded-5 fs-2 row bg-secondary w-50">
                 <div class="col-12 text-center">
-                    <p class="my-2">PRECIO FINAL: 140€ <button class="btn btn-comun rounded-5 fs-4 fst-italic">PROCEDER
+                    <p class="my-2">PRECIO FINAL: 140€ <button class="btn btn-comun rounded-pill fs-4 fst-italic">PROCEDER
                             AL PAGO</button></p>
                 </div>
             </div>
