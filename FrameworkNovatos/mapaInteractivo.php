aa105871f5c30374c5a8b90d98d095c9a814a9be
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2024-12-17 11:15:19 +0100

    Comienzo mapa interactivo

---
+++ FrameworkNovatos/mapaInteractivo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mapa Interactivo</title>
+</head>
+<body>
+    <main class="container mt-4">
+        <h1 class="text-center mb-4">Reservas de Mesa</h1>
+        <div class="row">
+            <div class="col-md-8">
+                <div id="contenedor-mapa"></div>
+            </div>
+            <div class="col-md-4">
+                <h3>Detalles de la Reserva</h3>
+                <ul class="list-group" id="reservation-list">
+                    <li class="list-group-item">Seleccione una mesa para reservar.</li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
