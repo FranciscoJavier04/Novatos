0420d8703c0cbc0517c01dd61437f33b9507f718
Alexro-03 <arodram24@iesmarquesdecomares.org>
2025-01-09 17:24:11 +0100

    cambios en las politicas

--- FrameworkNovatos/politicaCookies.php
+++ FrameworkNovatos/politicaCookies.php
@@ -14,70 +14,75 @@
 
         <p class="items-PC fs-2">Información sobre Cookies</p>
         <p class="item-PC fs-5">
-            En cumplimiento de la Ley 34/2002, de Servicios de la Sociedad de la Información y de Comercio Electrónico (LSSI),
-            y el Reglamento General de Protección de Datos (GDPR), ÓNEO RESTAURANTE S.L. obtiene el consentimiento expreso de los usuarios
-            antes de usar cookies no esenciales en su sitio web.
+            El acceso a este Sitio Web puede implicar la utilización de cookies. Las cookies son pequeñas cantidades de
+            información que se almacenan en el navegador utilizado por cada Usuario —en los distintos dispositivos que
+            pueda utilizar para navegar— para que el servidor recuerde cierta información que posteriormente y
+            únicamente el servidor que la implementó leerá. Las cookies facilitan la navegación, la hacen más amigable,
+            y no dañan el dispositivo de navegación.
         </p>
-
-        <p class="items-PC fs-2">¿Qué son las Cookies?</p>
         <p class="item-PC fs-5">
-            Las cookies son archivos que almacenan y recuperan información en el dispositivo del usuario para mejorar la navegación. 
-            Pueden recordar preferencias, datos de inicio de sesión y otros ajustes del sitio web.
+            Las cookies son procedimientos automáticos de recogida de información relativa a las preferencias
+            determinadas por el Usuario durante su visita al Sitio Web con el fin de reconocerlo como Usuario, y
+            personalizar su experiencia y el uso del Sitio Web, y pueden también, por ejemplo, ayudar a identificar y
+            resolver errores.
         </p>
-
-        <p class="items-PC fs-2">Cookies Afectadas por la Normativa</p>
         <p class="item-PC fs-5">
-            Requieren consentimiento las cookies de análisis, publicidad y afiliación. Las cookies técnicas y necesarias para el 
-            funcionamiento de la web están exceptuadas.
+            La información recabada a través de las cookies puede incluir la fecha y hora de visitas al Sitio Web, las
+            páginas visionadas, el tiempo que ha estado en el Sitio Web y los sitios visitados justo antes y después del
+            mismo. Sin embargo, ninguna cookie permite que esta misma pueda contactarse con el número de teléfono del
+            Usuario o con cualquier otro medio de contacto personal. Ninguna cookie puede extraer información del disco
+            duro del Usuario o robar información personal. La única manera de que la información privada del Usuario
+            forme parte del archivo Cookie es que el usuario dé personalmente esa información al servidor.
+        </p>
+        <p class="item-PC fs-5">
+            Las cookies que permiten identificar a una persona se consideran datos personales. Por tanto, a las mismas
+            les será de aplicación la Política de Privacidad anteriormente descrita. En este sentido, para la
+            utilización de las mismas será necesario el consentimiento del Usuario. Este consentimiento será comunicado,
+            en base a una elección auténtica, ofrecido mediante una decisión afirmativa y positiva, antes del
+            tratamiento inicial, removible y documentado.
         </p>
 
-        <p class="items-PC fs-2">Tipos de Cookies Según su Finalidad</p>
+        <p class="items-PC fs-2">Cookies propias</p>
+        <p class="item-PC fs-5">
+            Son aquellas cookies que son enviadas al ordenador o dispositivo del Usuario y gestionadas exclusivamente
+            por Óneo para el mejor funcionamiento del Sitio Web. La información que se recaba se emplea para mejorar la
+            calidad del Sitio Web y su Contenido y su experiencia como Usuario. Estas cookies permiten reconocer al
+            Usuario como visitante recurrente del Sitio Web y adaptar el contenido para ofrecerle contenidos que se
+            ajusten a sus preferencias.
+        </p>
+
+        <p class="items-PC fs-2">Cookies de redes sociales</p>
+        <p class="item-PC fs-5">
+            Óneo incorpora plugins de redes sociales, que permiten acceder a las mismas a partir del Sitio Web. Por esta
+            razón, las cookies de redes sociales pueden almacenarse en el navegador del Usuario. Los titulares de dichas
+            redes sociales disponen de sus propias políticas de protección de datos y de cookies, siendo ellos mismos,
+            en cada caso, responsables de sus propios ficheros y de sus propias prácticas de privacidad. El Usuario debe
+            referirse a las mismas para informarse acerca de dichas cookies y, en su caso, del tratamiento de sus datos
+            personales. Únicamente a título informativo se indican a continuación los enlaces en los que se pueden
+            consultar dichas políticas de privacidad y/o de cookies:
+        </p>
         <ul>
-            <li class="fs-5">Técnicas y Funcionales: Necesarias para la navegación y el uso de servicios.</li>
-            <li class="fs-5">Analíticas: Permiten analizar el comportamiento de navegación y mejorar el sitio web.</li>
-            <li class="fs-5">Publicitarias y de Publicidad Comportamental: Gestionan anuncios en función del comportamiento del usuario.</li>
-            <li class="fs-5">Sociales: Permiten compartir contenido en redes sociales.</li>
-            <li class="fs-5">De Afiliación: Hacen seguimiento de visitas desde otras webs asociadas.</li>
-            <li class="fs-5">De Seguridad: Protegen los datos almacenados en las cookies.</li>
+            <li class="fs-5"> Facebook: https://www.facebook.com/policies/cookies/</li>
+            <li class="fs-5"> Twitter: https://twitter.com/es/privacy</li>
+            <li class="fs-5"> Instagram: https://help.instagram.com/1896641480634370?ref=ig</li>
+            <li class="fs-5"> YouTube: https://policies.google.com/privacy?hl=es-419&gl=mx</li>
+            <li class="fs-5"> Pinterest: https://policy.pinterest.com/es/privacy-policy</li>
+            <li class="fs-5"> LinkedIn: https://www.linkedin.com/legal/cookie-policy?trk=hp-cookies</li>
         </ul>
 
-        <p class="items-PC fs-2">Tipos de Cookies Según la Propiedad</p>
-        <ul>
-            <li class="fs-5">Propias: Enviadas desde el dominio de ÓNEO.</li>
-            <li class="fs-5">De Terceros: Gestionadas por entidades externas.</li>
-        </ul>
-
-        <p class="items-PC fs-2">Tipos de Cookies Según el Plazo de Conservación</p>
-        <ul>
-            <li class="fs-5">De Sesión: Se eliminan al cerrar el navegador.</li>
-            <li class="fs-5">Persistentes: Permanecen almacenadas durante un tiempo determinado.</li>
-        </ul>
-
-        <p class="items-PC fs-2">Tratamiento de Datos Personales</p>
+        <p class="items-PC fs-2">Deshabilitar, rechazar y eliminar cookies</p>
         <p class="item-PC fs-5">
-            ÓNEO RESTAURANTE S.L. es el responsable del tratamiento de los datos personales recopilados a través de cookies, 
-            cumpliendo con el GDPR. Los usuarios pueden retirar su consentimiento en cualquier momento y tienen derecho a acceder, 
-            rectificar, portar y suprimir sus datos, o limitar su tratamiento.
+            El Usuario puede deshabilitar, rechazar y eliminar las cookies total o parcialmente instaladas en su
+            dispositivo mediante la configuración de su navegador (entre los que se encuentran, por ejemplo, Chrome,
+            Firefox, Safari, Explorer). En este sentido, los procedimientos para rechazar y eliminar las cookies pueden
+            diferir de un navegador de Internet a otro. En consecuencia, el Usuario debe acudir a las instrucciones
+            facilitadas por el propio navegador de Internet que esté utilizando. En el supuesto de que rechace el uso de
+            cookies —total o parcialmente— podrá seguir usando el Sitio Web, si bien podrá tener limitada la utilización
+            de algunas de las prestaciones del mismo.
         </p>
         <p class="item-PC fs-5">
-            Para ejercer estos derechos, los usuarios pueden contactar a: <br>
-            ÓNEO RESTAURANTE S.L.<br>
-            Dirección: Calle Francisco Villalón nº9, Marbella/España<br>
-            E-mail: protecciondatos@oneo.com</a>
-        </p>
-
-        <p class="items-PC fs-2">Cookies Utilizadas en Este Sitio Web</p>
-        <p class="item-PC fs-5">
-            El sitio web de ÓNEO puede usar cookies de terceros, cuyos proveedores gestionan sus propias políticas de privacidad. 
-            Entre estos proveedores se encuentran Google y PHP.net, cuyos enlaces a políticas de privacidad están disponibles 
-            en sus sitios web.
-        </p>
-
-        <p class="items-PC fs-2">Gestión de Cookies desde el Navegador</p>
-        <p class="item-PC fs-5">
-            Los usuarios pueden eliminar o bloquear las cookies desde la configuración de su navegador, aunque esto podría afectar 
-            el funcionamiento del sitio web. Las instrucciones para gestionar cookies en navegadores comunes como Chrome, Firefox, 
-            Safari, Edge y Opera están disponibles en sus respectivas páginas de soporte.
+            Este documento de Política de Cookies ha sido creado mediante el generador de plantilla de política de
+            cookies web gratis online el día 09/01/2025.
         </p>
     </div>
 
