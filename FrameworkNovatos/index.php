ee7729fa8d98facc54cd8421232706d5d7873523
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-01-11 19:39:17 +0100

    implementacion de los videos en la página principal

--- FrameworkNovatos/index.php
+++ FrameworkNovatos/index.php
@@ -18,10 +18,37 @@
         <div class="container py-5 ">
             <!-- Primera tarjeta -->
             <div class="row align-items-center mb-3">
-                <div class="col-md-4 p-0">
-                    <img src="assets/img/Logo.png" alt="Imagen 1" class="w-75">
+                <div class="col-md-6 p-0 d-flex flex-column align-items-center">
+                    <iframe id="youtube-player-1" class="video"
+                        src="https://www.youtube.com/embed/nSHrVn5ANZo?enablejsapi=1&controls=0"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen>
+                    </iframe>
+                    <div id="controls-1"
+                        class="px-1 controles my-0 d-flex align-items-center gap-2 flex-wrap bg-secondary text-primary">
+                        <button class="toggle-btn btn btn-primary d-flex align-items-center">
+                            <i class="play-icon fas fa-play"></i>
+                        </button>
+                        <button class="rewind-btn btn-secondary d-flex align-items-center">
+                            <i class="fas fa-backward"></i>
+                        </button>
+                        <label>15s</label>
+                        <button class="forward-btn btn-secondary d-flex align-items-center">
+                            <i class="fas fa-forward"></i>
+                        </button>
+                        <section class="progress-container d-flex align-items-center flex-grow-1 gap-2">
+                            <span class="current-time text-nowrap">0:00</span>
+                            <input type="range" class="progress-bar form-range flex-grow-1" min="0" max="100" value="0">
+                            <span class="total-time text-nowrap">0:00</span>
+                        </section>
+                        <section class="d-flex align-items-center gap-2">
+                            <i class="fas fa-volume-up"></i>
+                            <input type="range" class="volume-control form-range" min="0" max="100" value="50">
+                        </section>
+                    </div>
                 </div>
-                <div class="col-md-8 p-0">
+                <div class="col-md-6 p-0">
                     <p>
                         Estoy seguro de que has visto el Padrino, uno de los nuestros o cualquier película de Mafiosos.
                         Pues hoy os traigo Comiendo en el RESTAURANTE DE LA MAFIA en Marbella.
@@ -32,13 +59,40 @@
 
             <!-- Segunda tarjeta (invertida) -->
             <div class="row align-items-center my-3">
-                <div class="col-md-4 order-md-1 p-0">
-                    <img src="assets/img/Logo.png" alt="Imagen 2" class="w-75">
+                <div class="col-md-6 p-0 d-flex flex-column align-items-center order-md-1">
+                    <iframe id="youtube-player-2" class="video"
+                        src="https://www.youtube.com/embed/nSHrVn5ANZo?enablejsapi=1&controls=0"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen>
+                    </iframe>
+                    <div id="controls-2"
+                        class="px-1 controles my-0 d-flex align-items-center gap-2 flex-wrap bg-secondary text-primary">
+                        <button class="toggle-btn btn btn-primary d-flex align-items-center">
+                            <i class="play-icon fas fa-play"></i>
+                        </button>
+                        <button class="rewind-btn btn-secondary d-flex align-items-center">
+                            <i class="fas fa-backward"></i>
+                        </button>
+                        <label>15s</label>
+                        <button class="forward-btn btn-secondary d-flex align-items-center">
+                            <i class="fas fa-forward"></i>
+                        </button>
+                        <section class="progress-container d-flex align-items-center flex-grow-1 gap-2">
+                            <span class="current-time text-nowrap">0:00</span>
+                            <input type="range" class="progress-bar form-range flex-grow-1" min="0" max="100" value="0">
+                            <span class="total-time text-nowrap">0:00</span>
+                        </section>
+                        <section class="d-flex align-items-center gap-2">
+                            <i class="fas fa-volume-up"></i>
+                            <input type="range" class="volume-control form-range" min="0" max="100" value="50">
+                        </section>
+                    </div>
                 </div>
-                <div class="col-md-8 order-md-0 p-0">
+                <div class="col-md-6 p-0 order-md-0">
                     <p>
-                        Así es comer en uno de los restaurantes más lujosos de España, probamos todos los platos para
-                        dar nuestra opinión más sincera.
+                        Estoy seguro de que has visto el Padrino, uno de los nuestros o cualquier película de Mafiosos.
+                        Pues hoy os traigo Comiendo en el RESTAURANTE DE LA MAFIA en Marbella.
                     </p>
                 </div>
 
@@ -47,26 +101,46 @@
 
             <!-- Tercera tarjeta -->
             <div class="row align-items-center my-3">
-                <div class="col-md-4 p-0">
-                    <img src="assets/img/Logo.png" alt="Imagen 3" class="w-75">
+                <div class="col-md-6 p-0 d-flex flex-column align-items-center">
+                    <iframe id="youtube-player-3" class="video"
+                        src="https://www.youtube.com/embed/nSHrVn5ANZo?enablejsapi=1&controls=0"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen>
+                    </iframe>
+                    <div id="controls-3"
+                        class="px-1 controles my-0 d-flex align-items-center gap-2 flex-wrap bg-secondary text-primary">
+                        <button class="toggle-btn btn btn-primary d-flex align-items-center">
+                            <i class="play-icon fas fa-play"></i>
+                        </button>
+                        <button class="rewind-btn btn-secondary d-flex align-items-center">
+                            <i class="fas fa-backward"></i>
+                        </button>
+                        <label>15s</label>
+                        <button class="forward-btn btn-secondary d-flex align-items-center">
+                            <i class="fas fa-forward"></i>
+                        </button>
+                        <section class="progress-container d-flex align-items-center flex-grow-1 gap-2">
+                            <span class="current-time text-nowrap">0:00</span>
+                            <input type="range" class="progress-bar form-range flex-grow-1" min="0" max="100" value="0">
+                            <span class="total-time text-nowrap">0:00</span>
+                        </section>
+                        <section class="d-flex align-items-center gap-2">
+                            <i class="fas fa-volume-up"></i>
+                            <input type="range" class="volume-control form-range" min="0" max="100" value="50">
+                        </section>
+                    </div>
                 </div>
-                <div class="col-md-8 p-0">
-                    <p id="google_translate_element">
-                        La calidad y el buen servicio son nuestra razón de ser para que un cliente nos elija como su
-                        restaurante de referencia. Una filosofía llevada a nuestro día a día.
+                <div class="col-md-6 p-0">
+                    <p>
+                        Estoy seguro de que has visto el Padrino, uno de los nuestros o cualquier película de Mafiosos.
+                        Pues hoy os traigo Comiendo en el RESTAURANTE DE LA MAFIA en Marbella.
                     </p>
                 </div>
             </div>
             <div class="separator"></div>
         </div>
     </main>
-    <script type="text/javascript">
-        function googleTranslateElementInit() {
-            new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
-        }
-    </script>
-    <script type="text/javascript"
-        src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     <?php include("includes/footer.php"); ?>
 </body>
 
