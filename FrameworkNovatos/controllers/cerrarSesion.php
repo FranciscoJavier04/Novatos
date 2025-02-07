085b445f7558fbcfdcfabdcf03f5b236fd47ae4f
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-02-07 13:41:24 +0100

    cargar la sesion

--- FrameworkNovatos/controllers/cerrarSesion.php
+++ FrameworkNovatos/controllers/cerrarSesion.php
@@ -1,15 +1,12 @@
 <?php
-// cerrarSesion.php
-session_start(); // Inicia la sesión si no está iniciada
-
+session_start();
 // Destruye todas las variables de sesión
 $_SESSION = [];
 
 // Finalmente destruye la sesión
-
 session_destroy();
 
 // Redirige al usuario a la página de inicio de sesión u otra página deseada
-header("Location: ../index.php");
+header("Location: ../login.php");
 exit;
 ?>
